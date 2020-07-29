d03b105e2db956b31472b1b95806599b5104352a
RyanDBarnett <conner2468@gmail.com>
2020-07-29 09:25:58 -0600

    Add vendor name test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,10 +4,14 @@ require './lib/vendor'
 
 class VendorTest < Minitest::Test
   def setup
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor = Vendor.new('Rocky Mountain Fresh')
   end
 
   def test_it_exists
     assert_instance_of Vendor, @vendor
+  end
+
+  def test_has_name
+    assert_equal 'Rocky Mountain Fresh', @vendor.name
   end
 end
