8a2876ad76b1359c28a3fb90a3fdaf2b36195824
RyanDBarnett <conner2468@gmail.com>
2020-07-29 09:54:24 -0600

    Add market add vendor test

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,9 +6,13 @@ require './lib/market'
 
 class VendorTest < Minitest::Test
   def setup
-    # @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    # @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    # @vendor = Vendor.new('Rocky Mountain Fresh')
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @market = Market.new('South Pearl Street Farmers Market')
   end
 
@@ -22,5 +26,13 @@ class VendorTest < Minitest::Test
 
   def test_has_vendors
     assert_equal [], @market.vendors
+  end
+
+  def test_add_vendor
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 end
