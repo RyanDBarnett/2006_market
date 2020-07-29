ebbe843bee925079a090e542c3f6a867130601b4
RyanDBarnett <conner2468@gmail.com>
2020-07-29 09:39:22 -0600

    Add vendor stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,9 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < Minitest::Test
   def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @vendor = Vendor.new('Rocky Mountain Fresh')
   end
 
@@ -21,5 +24,25 @@ class VendorTest < Minitest::Test
 
   def test_check_stock
     assert_equal 0, @vendor.check_stock(@item1)
+  end
+
+  def test_stock
+    @vendor.stock(@item1, 30)
+
+    expected = {@item1 => 30}
+
+    assert_equal expected, @vendor.inventory
+
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+
+    expected = {@item1 => 55, @item2 => 12}
+
+    assert_equal expected, @vendor.inventory
   end
 end
